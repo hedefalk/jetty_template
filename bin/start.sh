11422be3c0a889c88dd870ace69df62b4a4f76f1
Viktor Hedefalk <hedefalk@gmail.com>
2013-04-26 16:17:42 +0200

    nohup

--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,7 @@
 
 cd $JETTY_HOME
 JETTY_CONFIGS="OPTIONS=Server,jmx,resources,ext,plus $JETTY_HOME/etc/jetty-jmx.xml $JETTY_HOME/etc/jetty.xml $JETTY_HOME/etc/jetty-deploy.xml $APP_HOME/etc/jetty-contexts.xml"
-java $JAVA_OPTIONS -jar start.jar $JETTY_CONFIGS >& $APP_HOME/logs/start.log &
+nohup java $JAVA_OPTIONS -jar start.jar $JETTY_CONFIGS >& $APP_HOME/logs/start.log &
 echo $! > $PID_FILE
 echo "The PID is $!"
 
