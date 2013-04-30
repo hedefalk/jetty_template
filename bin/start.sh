99bc5dbc4dc22665817eae5e9a1168afcad6c1d2
Viktor Hedefalk <hedefalk@gmail.com>
2013-04-30 12:06:14 +0300

    Jetty kills

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 . ./setupenv.sh
 
+#http://fuzzydb.blogspot.se/2012/04/jenkinshudson-killing-processes.html
+BUILD_ID=dontKillMe 
+
 cd $JETTY_HOME
 JETTY_CONFIGS="OPTIONS=Server,jmx,resources,ext,plus $JETTY_HOME/etc/jetty-jmx.xml $JETTY_HOME/etc/jetty.xml $JETTY_HOME/etc/jetty-deploy.xml $APP_HOME/etc/jetty-contexts.xml"
 nohup java $JAVA_OPTIONS -jar start.jar $JETTY_CONFIGS >& $APP_HOME/logs/start.log &
